937a6a1552e091076770e708b83e9459c7e90a09
Navis <navis.programmer@gmail.com>
2022-10-14 22:14:43 +0530

    product api with springboot

---
+++ src/main/java/com/navis/productapi/Product.java
@@ -0,0 +1,22 @@
+package com.navis.productapi;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+public class Product {
+    @Id
+    private String productId;
+    private String name;
+    private String code;
+    private String title;
+    private String description;
+    private String imgUrl;
+    private Double price;
+
+    public Product() {
+
+    }
+
+
+}
